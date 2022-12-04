6f3e6961787987906bcd7be03da773cb7248372a
PATRICIA PARK <pyjee8@google.com>
2022-12-04 22:03:59 +0800

    add missing data

--- mysite/csd/views.py
+++ mysite/csd/views.py
@@ -21,17 +21,41 @@ def index(request):
         year = datetime.today().year
         month = datetime.today().month
     
+    # 불러온 달의 첫째일과 마지막일
+    last_date = calendar.monthrange(int(year), int(month))[1]
+    first_day = year + '-' + month + '-01'
+    last_day = year+'-'+month+'-'+ str(last_date)
+    print(first_day, last_day)
+    
+    # testInx = DataFrame.reindex(dates, fill_value=0)
+
     # 빈 날짜 찾아서 list 넣기
     nullDate = []
+    result = []
+    
     def date_range(start, end):
         start = datetime.strptime(start, '%Y-%m-%d')
         end = datetime.strptime(end, '%Y-%m-%d')
         dates = [(start + timedelta(days=i+1)).strftime('%Y-%m-%d') for i in range((end-start).days-1)]
         return dates
     
+    
     test = SumDaily.objects.filter(prd_code = 11530035, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code')
     testValues = test.values_list('sum_d_date')
-    #print(test)
+    print('***testValues: ',test)
+    df = DataFrame(test)
+    # idx = pd.period_range(first_day, last_day)
+    print('***dfdfdf: ',df[0][0])
+    # df.reindex(idx, fill_value=0)
+    df['sum_d_date'] = pd.to_datetime(df['sum_d_date'])
+    df = (df.set_index('sum_d_date')
+        .reindex(pd.date_range(first_day, last_day, freq='D'))
+        .rename_axis(['sum_d_date'])
+        .fillna(0)
+        .reset_index())
+    
+    print('***df: ',df)
+    
     for i in range(1,len(testValues)):
         dates = date_range(str(testValues.get(pk=i))[15:27].replace(', ', '-').replace(')', ''), str(testValues.get(pk=i+1))[15:27].replace(', ', '-').replace(')', ''))
         #print(dates)
@@ -39,6 +63,7 @@ def index(request):
             for j in range(0,len(dates)):
                 #print(dates[j])
                 nullDate.append(dates[j])
+                # result.append(df.reindex(nullDate))
     
         
     #print(str(test.get(pk=1))[15:27].replace(', ', '-').replace(')', ''))
@@ -53,7 +78,9 @@ def index(request):
     prd17010002 = SumDaily.objects.filter(prd_code = 17010002, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code') #預購正官庄高麗蔘精ＥＶＥ / ＲＹＴＩＭＥ１０ｍｌ＊３０入
     
     #results = str(nullDate).replace('[', '').replace(']', '')
-    print('result ',nullDate)
+    
+    print('prd11530035 ',prd11530035)
+    # print('result ',result)
     # html 화면에 출력할 데이터 담을 리스트                    
     context = {'month':month,'nullDate':nullDate,'prd11530035':prd11530035, 'prd11060162':prd11060162, 'prd17010087':prd17010087, 'prd17010088':prd17010088, 'prd17010004':prd17010004, 'prd17010002':prd17010002}
     
@@ -100,6 +127,9 @@ def upload(request):
             date_year = str(int(str(ws.cell(row=2,column=1).value).split('：')[1].split('年')[0].strip())+1911)
             date_y_m_d = date_year + '-' + date_month + '-' + date_day
             fromdate_time_obj = datetime.strptime(date_y_m_d, '%Y-%m-%d')
+            
+            last_date = calendar.monthrange(int(date_year), int(date_month))[1]
+            print("******************",last_date)
             
             # date_null = int(date_day)
             # print("iiiiiiiiiiiii ", (i+1), date_null)
@@ -149,15 +179,14 @@ def upload(request):
                 d_sale  = wb.worksheets[i]['I4'].value
                 d_stock = wb.worksheets[i]['J4'].value
                 
-                # 비어있는 날짜 채우기
-                # first_day = fromdate_time_obj.replace(day=1)
-                # last_day = calendar.monthrange(date_year, date_month)[1]
-                # print('*****1st date& last date : ', first_day, last_day)
+                # Add missing dates to pandas dataframe
+                first_day = date_year+'-'+date_month+'-01'
+                last_day = date_year+'-'+date_month+'-'+ str(last_date)
+                idx = pd.date_range(first_day, last_day, freq='D')
+                df.reindex(idx, fill_value=0)
+                dfNull = df.reindex(index=idx, columns=['sum_d_save', 'sum_d_buy', 'sum_d_return', 'sum_d_sale', 'sum_d_stock'], fill_value=0)
                 
-                # btw_date = pd.date_range(first_day,last_day,freq='D')
-                # print('*****btw_date : ', btw_date)
-                
-                # 합계 데이터 DB 저장 (tbl_sum_d)
+                # Save sum data to DB (tbl_sum_d)
                 obj_sum, created = SumDaily.objects.update_or_create(
                     sum_d_date  = fromdate_time_obj,
                     sum_d_save  = d_save,
@@ -169,9 +198,7 @@ def upload(request):
                     str_code    = store
                     )
                 obj_sum.save()
-                #print(obj_sum)
                 
-                idx = pd.date_range()
             
                 # 엑셀 시트에 없는 날짜 추출 및 형변환
                 # print("iiiiiiiiiiiii ", (i+1), int(date_day))
