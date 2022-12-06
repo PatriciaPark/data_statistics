80fd9fab6bc144b9f7995452d329e40d0be6d84a
PATRICIA PARK <pyjee8@google.com>
2022-12-06 22:50:27 +0800

    total_avg_d

--- mysite/csd/views.py
+++ mysite/csd/views.py
@@ -20,68 +20,18 @@ def index(request):
         year = datetime.today().year
         month = datetime.today().month
     
-    # 불러온 달의 첫째일과 마지막일
     last_date = calendar.monthrange(int(year), int(month))[1]
-    # first_day = year + '-' + month + '-01'
-    # last_day = year+'-'+month+'-'+ str(last_date)
-    # print(first_day, last_day)
     
-    # testInx = DataFrame.reindex(dates, fill_value=0)
-
-    # 빈 날짜 찾아서 list 넣기
-    nullDate = []
-    result = []
-    
-    def date_range(start, end):
-        start = datetime.strptime(start, '%Y-%m-%d')
-        end = datetime.strptime(end, '%Y-%m-%d')
-        dates = [(start + timedelta(days=i+1)).strftime('%Y-%m-%d') for i in range((end-start).days-1)]
-        return dates
-    
-    
-    test = SumDaily.objects.filter(prd_code = 11530035, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code')
-    testValues = test.values_list('sum_d_date')
-    # print('***testValues: ',testValues)
-    # df = DataFrame(test)
-    # # idx = pd.period_range(first_day, last_day)
-    # print('***dfdfdf: ',df[0][0])
-    # # df.reindex(idx, fill_value=0)
-    # df['sum_d_date'] = pd.to_datetime(df['sum_d_date'])
-    # df = (df.set_index('sum_d_date')
-    #     .reindex(pd.date_range(first_day, last_day, freq='D'))
-    #     .rename_axis(['sum_d_date'])
-    #     .fillna(0)
-    #     .reset_index())
-    
-    # print('***df: ',df)
-    
-    # for i in range(1,len(testValues)):
-    #     dates = date_range(str(testValues.get(pk=i))[15:27].replace(', ', '-').replace(')', ''), str(testValues.get(pk=i+1))[15:27].replace(', ', '-').replace(')', ''))
-    #     print('*****dates', dates)
-    #     if dates:
-    #         for j in range(0,len(dates)):
-    #             #print(dates[j])
-    #             nullDate.append(dates[j])
-    #             # result.append(df.reindex(nullDate))
-    
-        
-    #print(str(test.get(pk=1))[15:27].replace(', ', '-').replace(')', ''))
-    # sumDaily = SumDaily.objects.select_related('prd_code', 'str_code').all()
-    # sumDaily = SumDaily.objects.filter(sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code')
     # DB에서 상품별 합계 데이터 받아오기
-    prd11530035 = SumDaily.objects.filter(prd_code = 11530035, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code') #正官庄活蔘２８Ｄ高麗蔘活 / 力飲１００ｍｌ＊１０瓶
-    prd11060162 = SumDaily.objects.filter(prd_code = 11060162, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code') #正官庄高麗蔘精ＥＶＥＲＹ / ＴＩＭＥ－秘１０ｍｌ＊２０入
-    prd17010087 = SumDaily.objects.filter(prd_code = 17010087, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code') #預購正官庄活蔘２８Ｄ高麗 / 蔘活力飲禮盒１００ｍｌ＊８入
-    prd17010088 = SumDaily.objects.filter(prd_code = 17010088, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code') #預購正官庄高麗蔘石榴飲 / ５０ｍｌ＊９入
-    prd17010004 = SumDaily.objects.filter(prd_code = 17010004, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code') #預購正官庄高麗蔘野櫻莓飲
-    prd17010002 = SumDaily.objects.filter(prd_code = 17010002, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code') #預購正官庄高麗蔘精ＥＶＥ / ＲＹＴＩＭＥ１０ｍｌ＊３０入
+    prd11530035 = list(SumDaily.objects.filter(prd_code = 11530035, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code').order_by('sum_d_date')) #正官庄活蔘２８Ｄ高麗蔘活 / 力飲１００ｍｌ＊１０瓶
+    prd11060162 = list(SumDaily.objects.filter(prd_code = 11060162, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code').order_by('sum_d_date')) #正官庄高麗蔘精ＥＶＥＲＹ / ＴＩＭＥ－秘１０ｍｌ＊２０入
+    prd17010087 = list(SumDaily.objects.filter(prd_code = 17010087, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code').order_by('sum_d_date')) #預購正官庄活蔘２８Ｄ高麗 / 蔘活力飲禮盒１００ｍｌ＊８入
+    prd17010088 = list(SumDaily.objects.filter(prd_code = 17010088, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code').order_by('sum_d_date')) #預購正官庄高麗蔘石榴飲 / ５０ｍｌ＊９入
+    prd17010004 = list(SumDaily.objects.filter(prd_code = 17010004, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code').order_by('sum_d_date')) #預購正官庄高麗蔘野櫻莓飲
+    prd17010002 = list(SumDaily.objects.filter(prd_code = 17010002, sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code').order_by('sum_d_date')) #預購正官庄高麗蔘精ＥＶＥ / ＲＹＴＩＭＥ１０ｍｌ＊３０入
     
-    #results = str(nullDate).replace('[', '').replace(']', '')
-    
-    # print('prd11530035 ',prd11530035)
-    # print('result ',result)
     # html 화면에 출력할 데이터 담을 리스트                    
-    context = {'month':month,'nullDate':nullDate,'prd11530035':prd11530035, 'prd11060162':prd11060162, 'prd17010087':prd17010087, 'prd17010088':prd17010088, 'prd17010004':prd17010004, 'prd17010002':prd17010002}
+    context = {'month':month, 'last_date':last_date, 'prd11530035':prd11530035, 'prd11060162':prd11060162, 'prd17010087':prd17010087, 'prd17010088':prd17010088, 'prd17010004':prd17010004, 'prd17010002':prd17010002}
     
     return render(request, 'csd/index.html', context)
 
@@ -135,74 +85,73 @@ def upload(request):
                 saved_date.append(int(sheetname[k].split('.')[1]))
 
             none_date = list(set(all_date)-set(saved_date))
-            print('*****none_date',none_date)
-####################### 여기서부터 수정
-            # date_y_m_d_null = date_year + '-' + date_month + '-' + none_date
-            # fromdate_time_obj_null = datetime.strptime(date_y_m_d_null, '%Y-%m-%d')
-            # print("fromdate_time_obj_null", fromdate_time_obj_null)
+            # print('*****none_date',none_date)
+            for l in range(0,len(none_date)):
+                date_y_m_d_null = date_year + '-' + date_month + '-' + str(none_date[l])
+                fromdate_time_obj_null = datetime.strptime(date_y_m_d_null, '%Y-%m-%d')
+                # print("fromdate_time_obj_null", fromdate_time_obj_null)      
+            
           
-        # 데이터 DB 저장            
-        for dbframe in df.itertuples():
-            # 제품/매장 테이블에서 외래키 참조
-            product = 0
-            
-            try:
-                product = Product.objects.get(prd_code = str(dbframe.貨號))
-            except ObjectDoesNotExist:
-                product = None
+            # 데이터 DB 저장            
+            for dbframe in df.itertuples():
+                # 제품/매장 테이블에서 외래키 참조
+                product = 0
                 
-            # 일일 데이터 DB 저장 (tbl_invoice_d)
-            # obj, created = InvoiceDaily.objects.update_or_create(
-            #     inv_d_date  = fromdate_time_obj,
-            #     inv_d_save  = dbframe.上存量,
-            #     inv_d_buy   = dbframe.進貨量,
-            #     inv_d_return= dbframe.退貨量,
-            #     inv_d_sale  = dbframe.銷貨量,
-            #     inv_d_stock = dbframe.庫存量,
-            #     prd_code    = product,
-            #     str_code    = store
-            #     )
-            # obj.save()
-            
-            # 엑셀 합계 데이터 들어있는 셀 읽어오기 (F4~J4)
-            d_save  = wb.worksheets[i]['F4'].value
-            d_buy   = wb.worksheets[i]['G4'].value
-            d_return= wb.worksheets[i]['H4'].value
-            d_sale  = wb.worksheets[i]['I4'].value
-            d_stock = wb.worksheets[i]['J4'].value
-            
-            # Add missing dates to pandas dataframe
-            # first_day = date_year+'-'+date_month+'-01'
-            # last_day = date_year+'-'+date_month+'-'+ str(last_date)
-            # idx = pd.date_range(first_day, last_day, freq='D')
-            # df.reindex(idx, fill_value=0)
-            # dfNull = df.reindex(index=idx, columns=['sum_d_save', 'sum_d_buy', 'sum_d_return', 'sum_d_sale', 'sum_d_stock'], fill_value=0)
-            
-            # Save sum data to DB (tbl_sum_d)
-            obj_sum, created = SumDaily.objects.update_or_create(
-                sum_d_date  = fromdate_time_obj,
-                sum_d_save  = d_save,
-                sum_d_buy   = d_buy,
-                sum_d_return= d_return,
-                sum_d_sale  = d_sale,
-                sum_d_stock = d_stock,
-                prd_code    = product
-                # str_code    = store
-                )
-            obj_sum.save()
-            
-                  
-            # 없는 날짜 데이터 넣기
-            obj_none, created = SumDaily.objects.update_or_create(
-                sum_d_date  = fromdate_time_obj_null,
-                sum_d_save  = 0,
-                sum_d_buy   = 0,
-                sum_d_return= 0,
-                sum_d_sale  = 0,
-                sum_d_stock = 0,
-                prd_code    = product
-                )
-            obj_none.save()
+                try:
+                    product = Product.objects.get(prd_code = str(dbframe.貨號))
+                except ObjectDoesNotExist:
+                    product = None
                     
-        
+                # 일일 데이터 DB 저장 (tbl_invoice_d)
+                # obj, created = InvoiceDaily.objects.update_or_create(
+                #     inv_d_date  = fromdate_time_obj,
+                #     inv_d_save  = dbframe.上存量,
+                #     inv_d_buy   = dbframe.進貨量,
+                #     inv_d_return= dbframe.退貨量,
+                #     inv_d_sale  = dbframe.銷貨量,
+                #     inv_d_stock = dbframe.庫存量,
+                #     prd_code    = product,
+                #     str_code    = store
+                #     )
+                # obj.save()
+                
+                # 엑셀 합계 데이터 들어있는 셀 읽어오기 (F4~J4)
+                d_save  = wb.worksheets[i]['F4'].value
+                d_buy   = wb.worksheets[i]['G4'].value
+                d_return= wb.worksheets[i]['H4'].value
+                d_sale  = wb.worksheets[i]['I4'].value
+                d_stock = wb.worksheets[i]['J4'].value
+                
+                # Add missing dates to pandas dataframe
+                # first_day = date_year+'-'+date_month+'-01'
+                # last_day = date_year+'-'+date_month+'-'+ str(last_date)
+                # idx = pd.date_range(first_day, last_day, freq='D')
+                # df.reindex(idx, fill_value=0)
+                # dfNull = df.reindex(index=idx, columns=['sum_d_save', 'sum_d_buy', 'sum_d_return', 'sum_d_sale', 'sum_d_stock'], fill_value=0)
+                
+                # Save sum data to DB (tbl_sum_d)
+                obj_sum, created = SumDaily.objects.update_or_create(
+                    sum_d_date  = fromdate_time_obj,
+                    sum_d_save  = d_save,
+                    sum_d_buy   = d_buy,
+                    sum_d_return= d_return,
+                    sum_d_sale  = d_sale,
+                    sum_d_stock = d_stock,
+                    prd_code    = product
+                    # str_code    = store
+                    )
+                obj_sum.save()
+               
+                # 없는 날짜 데이터 넣기
+                obj_none, created = SumDaily.objects.update_or_create(
+                    sum_d_date  = fromdate_time_obj_null,
+                    sum_d_save  = 0,
+                    sum_d_buy   = 0,
+                    sum_d_return= 0,
+                    sum_d_sale  = 0,
+                    sum_d_stock = 0,
+                    prd_code    = product
+                    )
+                obj_none.save()
+                    
     return redirect('/csd/?input-month='+date_year+'-'+date_month)
