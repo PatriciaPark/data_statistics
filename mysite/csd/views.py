e647c1d1ce1ab361ad59227c251522c6c0c130ce
YJP <pat0721@naver.com>
2022-12-06 17:24:38 +0800

    monthly complete

--- mysite/csd/views.py
+++ mysite/csd/views.py
@@ -1,12 +1,11 @@
 #-*-coding: utf-8
 from django.shortcuts import render, redirect
 from .models import *
-import calendar
-from datetime import datetime, timedelta
 from django.core.files.storage import FileSystemStorage
 from django.core.exceptions import ObjectDoesNotExist
-import openpyxl
+from datetime import datetime, timedelta
 from pandas import DataFrame
+import openpyxl, calendar
 import pandas as pd
 
 
@@ -96,21 +95,16 @@ def upload(request):
         # 파일명에 한자가 들어가 있으면 위 코드 사용시 인코딩 문제로 에러 발생
         excel_file = '/media/'+ filename
         
+        #시트 이름 가져오기
         wb = openpyxl.load_workbook('.'+excel_file, data_only=True)
         ws = wb.worksheets[0]
-        #시트 이름 가져오기
         sheetname = wb.sheetnames
+        
         # Converting a worksheet to a Dataframe
         data = ws.values
-        
-        #print('************************wb.worksheets[0] ',wb.worksheets[0])
-        #print('************************wb.worksheets[1] ',wb.worksheets[1])
-        #print('************************sheetname ', len(sheetname))
-
         # 필드명(첫행) 뽑아내어 cols에 넣어주고 뽑아낸 행은 data에서 삭제된다.
         # 여기서 next()는 한행 한행 iter 해주는 역할을 하고
         # [0:]은 필드를 col기준 어디서부터 선택할 것인지이다. 예)[1:]는 두번째 col부터 선택
-
         cols = next(data)[0:]
         cols = next(data)[0:]
         cols = next(data)[0:]
@@ -119,7 +113,7 @@ def upload(request):
         df = DataFrame(data, columns=cols)
         
         for i in range(0, len(sheetname)):
-            # 엑셀 시트 이름에서 날짜 추출 
+            # 엑셀 sheet 이름에서 날짜 추출 
             date_month  = str(sheetname[i].split('.')[0])
             date_day    = str(sheetname[i].split('.')[1])
             # 추출한 날짜 형변환
@@ -127,119 +121,88 @@ def upload(request):
             date_y_m_d = date_year + '-' + date_month + '-' + date_day
             fromdate_time_obj = datetime.strptime(date_y_m_d, '%Y-%m-%d')
             
-            # last_date = calendar.monthrange(int(date_year), int(date_month))[1]
+            last_date = calendar.monthrange(int(date_year), int(date_month))[1]
             print("****************** ",fromdate_time_obj)
             
-            # date_null = int(date_day)
-            # print("iiiiiiiiiiiii ", (i+1), date_null)
-            # if i+1 != date_null:
-            #     NullDate = i+1
-            #     overDate = date_null
-            #     print("************NullDate: ", str(NullDate))
-            #     date_y_m_d_null = date_year + '-' + date_month + '-' + str(NullDate)
-            #     fromdate_time_obj_null = datetime.strptime(date_y_m_d_null, '%Y-%m-%d')
-            #     print("fromdate_time_obj_null", fromdate_time_obj_null)
-            #     date_null = overDate-1
-            #     continue
+            # 엑셀 시트에 없는 날짜 추출 및 형변환
+            all_date = []
+            saved_date = []
+            
+            for j in range(last_date):
+                all_date.append(j+1)
+                
+            for k in range(0, len(sheetname)):     
+                saved_date.append(int(sheetname[k].split('.')[1]))
+
+            none_date = list(set(all_date)-set(saved_date))
+            print('*****none_date',none_date)
+####################### 여기서부터 수정
+            # date_y_m_d_null = date_year + '-' + date_month + '-' + none_date
+            # fromdate_time_obj_null = datetime.strptime(date_y_m_d_null, '%Y-%m-%d')
+            # print("fromdate_time_obj_null", fromdate_time_obj_null)
           
-            # 데이터 DB 저장
-            for dbframe in df.itertuples():
-                # 제품/매장 테이블에서 외래키 참조
-                product = 0
-                
-                try:
-                    product = Product.objects.get(prd_code = str(dbframe.貨號))
-                except ObjectDoesNotExist:
-                    product = None
-                    
-                # try:
-                #     store = Store.objects.get(str_code = str(dbframe.門市代號))
-                # except ObjectDoesNotExist:
-                #     store = None
-                    
-                # 일일 데이터 DB 저장 (tbl_invoice_d)
-                # obj, created = InvoiceDaily.objects.update_or_create(
-                #     inv_d_date  = fromdate_time_obj,
-                #     inv_d_save  = dbframe.上存量,
-                #     inv_d_buy   = dbframe.進貨量,
-                #     inv_d_return= dbframe.退貨量,
-                #     inv_d_sale  = dbframe.銷貨量,
-                #     inv_d_stock = dbframe.庫存量,
-                #     prd_code    = product,
-                #     str_code    = store
-                #     )
-                # obj.save()
-                
-                # 엑셀 합계 데이터 들어있는 셀 읽어오기 (F4~J4)
-                d_save  = wb.worksheets[i]['F4'].value
-                d_buy   = wb.worksheets[i]['G4'].value
-                d_return= wb.worksheets[i]['H4'].value
-                d_sale  = wb.worksheets[i]['I4'].value
-                d_stock = wb.worksheets[i]['J4'].value
-                
-                # Add missing dates to pandas dataframe
-                # first_day = date_year+'-'+date_month+'-01'
-                # last_day = date_year+'-'+date_month+'-'+ str(last_date)
-                # idx = pd.date_range(first_day, last_day, freq='D')
-                # df.reindex(idx, fill_value=0)
-                # dfNull = df.reindex(index=idx, columns=['sum_d_save', 'sum_d_buy', 'sum_d_return', 'sum_d_sale', 'sum_d_stock'], fill_value=0)
-                
-                # Save sum data to DB (tbl_sum_d)
-                obj_sum, created = SumDaily.objects.update_or_create(
-                    sum_d_date  = fromdate_time_obj,
-                    sum_d_save  = d_save,
-                    sum_d_buy   = d_buy,
-                    sum_d_return= d_return,
-                    sum_d_sale  = d_sale,
-                    sum_d_stock = d_stock,
-                    prd_code    = product
-                    # str_code    = store
-                    )
-                obj_sum.save()
-                
+        # 데이터 DB 저장            
+        for dbframe in df.itertuples():
+            # 제품/매장 테이블에서 외래키 참조
+            product = 0
             
-                # 엑셀 시트에 없는 날짜 추출 및 형변환
-                # print("iiiiiiiiiiiii ", (i+1), int(date_day))
-                # if i+1 != int(date_day):
-                #     NullDate = int(date_day)
-                #     print("************NullDate: ", str(NullDate))
-                #     date_y_m_d_null = date_year + '-' + date_month + '-' + str(NullDate)
-                #     fromdate_time_obj_null = datetime.strptime(date_y_m_d_null, '%Y-%m-%d')
-                #     print("fromdate_time_obj_null", fromdate_time_obj_null)
-                    
-                    # 없는 날짜 데이터 넣기
-                    # obj_sum, created = SumDaily.objects.update_or_create(
-                    #     sum_d_date  = fromdate_time_obj_null,
-                    #     sum_d_save  = 0,
-                    #     sum_d_buy   = 0,
-                    #     sum_d_return= 0,
-                    #     sum_d_sale  = 0,
-                    #     sum_d_stock = 0,
-                    #     prd_code    = product,
-                    #     str_code    = store
-                    #     )
-                    # obj_sum.save()
-                    
+            try:
+                product = Product.objects.get(prd_code = str(dbframe.貨號))
+            except ObjectDoesNotExist:
+                product = None
                 
-            
-                
-            # 직접 합계 계산하는 로직 => 오래걸림
-            # sum_save = InvoiceDaily.objects.filter(inv_d_date=date_y_m_d).aggregate(Sum('inv_d_save'))
-            # sum_buy = InvoiceDaily.objects.filter(inv_d_date=date_y_m_d).aggregate(Sum('inv_d_buy'))
-            # sum_return = InvoiceDaily.objects.filter(inv_d_date=date_y_m_d).aggregate(Sum('inv_d_return'))
-            # sum_sale = InvoiceDaily.objects.filter(inv_d_date=date_y_m_d).aggregate(Sum('inv_d_sale'))
-            # sum_stock = InvoiceDaily.objects.filter(inv_d_date=date_y_m_d).aggregate(Sum('inv_d_stock'))
-            
-            # obj2 = SumDaily.objects.create(
-            #     sum_d_date  = fromdate_time_obj,
-            #     sum_d_save  = sum_save['inv_d_save__sum'],
-            #     sum_d_buy   = sum_buy['inv_d_buy__sum'],
-            #     sum_d_return= sum_return['inv_d_return__sum'],
-            #     sum_d_sale  = sum_sale['inv_d_sale__sum'],
-            #     sum_d_stock = sum_stock['inv_d_stock__sum'],
+            # 일일 데이터 DB 저장 (tbl_invoice_d)
+            # obj, created = InvoiceDaily.objects.update_or_create(
+            #     inv_d_date  = fromdate_time_obj,
+            #     inv_d_save  = dbframe.上存量,
+            #     inv_d_buy   = dbframe.進貨量,
+            #     inv_d_return= dbframe.退貨量,
+            #     inv_d_sale  = dbframe.銷貨量,
+            #     inv_d_stock = dbframe.庫存量,
             #     prd_code    = product,
             #     str_code    = store
-            # )
-            # obj2.save()
+            #     )
+            # obj.save()
+            
+            # 엑셀 합계 데이터 들어있는 셀 읽어오기 (F4~J4)
+            d_save  = wb.worksheets[i]['F4'].value
+            d_buy   = wb.worksheets[i]['G4'].value
+            d_return= wb.worksheets[i]['H4'].value
+            d_sale  = wb.worksheets[i]['I4'].value
+            d_stock = wb.worksheets[i]['J4'].value
+            
+            # Add missing dates to pandas dataframe
+            # first_day = date_year+'-'+date_month+'-01'
+            # last_day = date_year+'-'+date_month+'-'+ str(last_date)
+            # idx = pd.date_range(first_day, last_day, freq='D')
+            # df.reindex(idx, fill_value=0)
+            # dfNull = df.reindex(index=idx, columns=['sum_d_save', 'sum_d_buy', 'sum_d_return', 'sum_d_sale', 'sum_d_stock'], fill_value=0)
+            
+            # Save sum data to DB (tbl_sum_d)
+            obj_sum, created = SumDaily.objects.update_or_create(
+                sum_d_date  = fromdate_time_obj,
+                sum_d_save  = d_save,
+                sum_d_buy   = d_buy,
+                sum_d_return= d_return,
+                sum_d_sale  = d_sale,
+                sum_d_stock = d_stock,
+                prd_code    = product
+                # str_code    = store
+                )
+            obj_sum.save()
+            
+                  
+            # 없는 날짜 데이터 넣기
+            obj_none, created = SumDaily.objects.update_or_create(
+                sum_d_date  = fromdate_time_obj_null,
+                sum_d_save  = 0,
+                sum_d_buy   = 0,
+                sum_d_return= 0,
+                sum_d_sale  = 0,
+                sum_d_stock = 0,
+                prd_code    = product
+                )
+            obj_none.save()
+                    
         
     return redirect('/csd/?input-month='+date_year+'-'+date_month)
--- mysite/csm/views.py
+++ mysite/csm/views.py
@@ -1,53 +1,51 @@
 #-*-coding: utf-8
 from django.shortcuts import render, redirect
 from .models import *
-import calendar
-from datetime import datetime, timedelta
 from django.core.files.storage import FileSystemStorage
 from django.core.exceptions import ObjectDoesNotExist
-import openpyxl
+from datetime import datetime
 from pandas import DataFrame
-import pandas as pd
+import openpyxl, calendar
 
 
-# Create your views here.
+# index
 def index(request):
-    # html에서 선택한 날짜 받아오기
-    try:
-        date = request.GET.get('input-month')   #2022-11
-        year = date[:4]     #2022
-        month = date[5:]    #11
-    except TypeError:
-        year = datetime.today().year
-        month = datetime.today().month
+  # html에서 선택한 날짜 받아오기
+  try:
+      date = request.GET.get('input-month')   #2022-11
+      year = date[:4]     #2022
+      month = date[5:]    #11
+  except TypeError:
+      year = datetime.today().year
+      month = datetime.today().month
 
-    monthData = SumMonthly.objects.filter(sum_d_date__year=int(year), sum_d_date__month=int(month)).select_related('prd_code','str_code')
-
-
-    # html 화면에 출력할 데이터 담을 리스트                    
-    context = {'month':month, 'monthData':monthData}
+  monthData = SumMonthly.objects.filter(sum_m_date__year=int(year), sum_m_date__month=int(month)).select_related('prd_code','str_code')
     
-    return render(request, 'csm/index.html', context)
+  # html 화면에 출력할 데이터 담을 리스트                    
+  context = {'month':month, 'monthData':monthData}
 
+  return render(request, 'csm/index.html', context)
+
+# excel file upload
 def upload(request):
-    if request.method == 'POST' and request.FILES['fileInput']:
-      # file directory
-      file = request.FILES['fileInput']
-      fs = FileSystemStorage()
-      filename = fs.save(file.name.replace(' ' , ''), file)
-      excel_file = '/media/'+ filename
-      
-      # get sheet name
-      wb = openpyxl.load_workbook('.'+excel_file, data_only=True)
-      ws = wb.worksheets[0]
+  if request.method == 'POST' and request.FILES['fileInput']:
+    # file directory
+    file = request.FILES['fileInput']
+    fs = FileSystemStorage()
+    filename = fs.save(file.name.replace(' ' , ''), file)
+    excel_file = '/media/'+ filename
+    
+    # get sheet name
+    wb = openpyxl.load_workbook('.'+excel_file, data_only=True)
+    ws = wb.worksheets[0]
+    sheetname = wb.sheetnames
+    
+    for i in range(0, len(sheetname)):
+      ws = wb.worksheets[i]
       sheetname = wb.sheetnames
+      
       # Converting a worksheet to a Dataframe
       data = ws.values
-
-      # 필드명(첫행) 뽑아내어 cols에 넣어주고 뽑아낸 행은 data에서 삭제된다.
-      # 여기서 next()는 한행 한행 iter 해주는 역할을 하고
-      # [0:]은 필드를 col기준 어디서부터 선택할 것인지이다. 예)[1:]는 두번째 col부터 선택
-
       cols = next(data)[0:]
       cols = next(data)[0:]
       cols = next(data)[0:]
@@ -55,44 +53,52 @@ def upload(request):
       cols = next(data)[0:]
       df = DataFrame(data, columns=cols)
       
-      # date_day    = str(filename[3:5].strip())
-      print("filename", filename.split(".")[0].split('_')[0][-2:].strip())
+      # 날짜 추출      
       date_month  = str(filename.split(".")[0].split('_')[0][-2:].strip())
-      print('int(filename[-2:]): ', date_month)
       date_year = str(int(str(ws.cell(row=2,column=1).value).split("：")[1].split("年")[0].strip())+1911)
-      print('date_year ',date_year)
-
-      date_y_m_d = date_year + "-" + date_month
-      print("date_y_m_d: ", date_y_m_d)    
-      fromdate_time_obj = datetime.strptime(date_y_m_d, '%Y-%m')
-      print("fromdate_time_obj: ",fromdate_time_obj)
-              
+      date_y_m = date_year + "-" + date_month
+      fromdate_time_obj = datetime.strptime(date_y_m, '%Y-%m')
+      
+      # 불러온 달의 마지막일 (eg. 28일/30일/31일)
+      last_date = calendar.monthrange(int(date_year), int(date_month))[1]
+      
       # 데이터 DB 저장
       for dbframe in df.itertuples():
           # 제품/매장 테이블에서 외래키 참조
           product, store = 0,0
           
           try:
-              product = Product.objects.get(prd_code = str(dbframe.貨號))
+            product = Product.objects.get(prd_code = str(dbframe.貨號))
           except ObjectDoesNotExist:
-              product = None
+            product = None
               
           try:
-              store = Store.objects.get(str_code = str(dbframe.門市代號))
+            store = Store.objects.get(str_code = str(dbframe.門市代號))
           except ObjectDoesNotExist:
-              store = None
-              
+            store = None
+          
+          # 일일 판매량 및 오차율    
+          m_sale  = wb.worksheets[i]['K4'].value # 판매총합 셀
+          dailySale, errorRate = 0, 0
+          dailySale = float(format(m_sale/last_date,".2f"))
+          try : 
+            errorRate = format(dbframe.銷貨量/dailySale,".2f")
+          except ZeroDivisionError:
+            errorRate = None
+          
           # Save sum data to DB (tbl_sum_m)
-          obj_sum, created = SumMonthly.objects.update_or_create(
+          obj, created = SumMonthly.objects.update_or_create(
               sum_m_date  = fromdate_time_obj,
               sum_m_save  = dbframe.上存量,
               sum_m_buy   = dbframe.進貨量,
               sum_m_return= dbframe.退貨量,
               sum_m_sale  = dbframe.銷貨量,
               sum_m_stock = dbframe.庫存量,
+              sum_m_sale_ttl = dailySale,
+              sum_m_sale_err = errorRate,
               prd_code    = product,
               str_code    = store
               )
-          obj_sum.save()
-               
-    return redirect('/csm/?input-month='+date_year+'-'+date_month)
+          obj.save()
+              
+  return redirect('/csm/?input-month='+date_year+'-'+date_month)
