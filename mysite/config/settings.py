eda305c48c4a4c0e168788cae974fc1322973203
PATRICIA PARK <pyjee8@google.com>
2022-12-07 21:29:01 +0800

    loading img

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'config', 'static'),
   os.path.join(BASE_DIR, 'csd', 'static'),
   os.path.join(BASE_DIR, 'csm', 'static'),
-  os.path.join(BASE_DIR, 'user', 'static'),
+  os.path.join(BASE_DIR, 'users', 'static'),
 ]
 
 MEDIA_URL = '/media/'
