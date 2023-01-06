1a174a9d682bbe98051b3050c2323e21afab57cb
YJP <pat0721@naver.com>
2023-01-06 17:14:10 +0800

    aws e2c

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-#g^i!=wf@4f*0zh5#=9hv)yt%*99w%$_26v!3m(16-lt^0c@#&
 DEBUG = False
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['13.124.44.214']
 
 
 # Application definition
