9d61fc259adc912f9ef87d2c13634d0d5fd71fdd
YJP <pat0721@naver.com>
2023-01-06 14:57:33 +0800

    aws

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -84,15 +84,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'work_automation',
+#         'USER': 'root',
+#         'PASSWORD': 'dudwlsla',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'work_automation',
-        'USER': 'root',
-        'PASSWORD': 'dudwlsla',
-        'HOST': 'localhost',
+        'NAME': 'kgc-chbs',
+        'USER': 'admin',
+        'PASSWORD': 'qweQWE123!#',
+        'HOST': 'kgc-chbs.cnmgxmzrayoa.ap-northeast-2.rds.amazonaws.com',
         'PORT': '3306',
     }
 }
