7dc49a5b2a90d62b0c9cbd25d67828283135deae
YJP <pat0721@naver.com>
2023-01-07 16:52:45 +0800

    cors setting

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -48,9 +48,11 @@ INSTALLED_APPS = [
     'csy',
     'csp',
     'users',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
