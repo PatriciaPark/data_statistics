e2df5e1e6be76b80c6607c1fab64e79bd6a33b1e
YJP <pat0721@naver.com>
2023-01-07 16:06:39 +0800

    url re path

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.views.static import serve
+from django.urls import path, include, re_path
 from django.conf import settings
 from .views import *
 from django.conf.urls import handler400, handler404, handler500
@@ -36,3 +37,6 @@ urlpatterns = [
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
