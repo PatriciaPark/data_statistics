b2273622bee8f12866858373e3a73223502057b4
YJP <pat0721@naver.com>
2023-01-18 10:21:55 +0800

    settings

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -156,7 +156,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'config', 'static'),
     os.path.join(BASE_DIR, 'csd', 'static'),
     os.path.join(BASE_DIR, 'csm', 'static'),
